cd260d3a4ac8ccfa601b66b5377d09bb9ae8e3bf
junior.winkler <juniorgalhardo03@gmail.com>
2026-09-23 13:57:20 -0300

    testar deploy automatico no test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Ok(new { status = "Healthy", version = "v1.0.0", message = "API v1 - DEPLOY AUTOMÁTICO FUNCIONANDO!" }));
+app.MapGet("/", () => Results.Ok(new { status = "Healthy", version = "v1.0.0", message = "API 2 v1 - DEPLOY AUTOMÁTICO FUNCIONANDO!" }));
 
 app.Run();
