e87d707903315a818b053804748c7af11b62ba2e
junior.winkler <juniorgalhardo03@gmail.com>
2026-09-23 15:28:22 -0300

    Adicionar endpoint exclusivo da homologacao

--- Program.cs
+++ Program.cs
@@ -2,5 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => Results.Ok(new { status = "Healthy", version = "v1.0.0", message = "API 2 v1 - DEPLOY AUTOMÁTICO FUNCIONANDO!" }));
-
+app.MapGet("/homolog-only", () => new
+{
+    status = "OK",
+    environment = "HOMOLOG",
+    message = "Esta funcionalidade existe somente na branch develop!"
+});
 app.Run();
