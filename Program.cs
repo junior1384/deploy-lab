9c024aa596f4144486e8e4f1d08a15f74106a225
junior.winkler <juniorgalhardo03@gmail.com>
2026-09-23 13:43:22 -0300

    deploy_x2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Ok(new { status = "Healthy", version = "v1.0.0", message = "API v1 no ar!" }));
+app.MapGet("/", () => Results.Ok(new { status = "Healthy", version = "v1.0.0", message = "API v1 - DEPLOY AUTOMÁTICO FUNCIONANDO!" }));
 
 app.Run();
